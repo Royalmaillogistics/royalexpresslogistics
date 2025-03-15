dd8320e8878e61490d92db73eb5620ea0e1c12c9
Royalmaillogistics <9jaintaiwan@gmail.com>
2025-03-15 21:52:08 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "62090956"
+    serviceWorkerVersion: "1984818001"
   }
 });

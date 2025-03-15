4e522c356da1fa6bc0974ca616441ef3fd36ab96
Royalmaillogistics <9jaintaiwan@gmail.com>
2025-03-15 18:12:17 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a2f3f9d20553698d7759367325d2e2fe
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "68f7a35ceeb93ac891dd63e4e3bc394d",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "fb4ef15546a8f5b243fda45343e3a86c",
+"flutter_bootstrap.js": "2a6ee3d4cd412d5847a3282a325fd8d2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
